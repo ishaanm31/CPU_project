0d06cb56be815d0c9055e510158948f48a11bed2
Ishaan Manhar <210070033@iitb.ac.in>
2022-11-25 20:57:14 +0530

    yup

--- Datapath.vhdl
+++ Datapath.vhdl
@@ -11,11 +11,14 @@ entity Datapath is
         A1_sel : in std_logic_vector(0 downto 0);
         A3_sel : in std_logic_vector(1 downto 0);
         D3_sel : in std_logic_vector(1 downto 0);
-        Reg
+        Reg_file_EN: in std_logic;
+        C_ctrl, Z_ctrl: in std_logic;
+        T1_WR,T2_WR,T3_WR: in std_logic;
+        sel_m1,sel_m2,sel_m3: in std_logic;
+        
         --Outputs
         Mem_Data_In: out std_logic_vector(15 downto 0);
-        
-
+        Z_flag, C_flag: out std_logic;
 		);
 end Datapath;
 
@@ -83,7 +86,7 @@ architecture Struct of Datapath is
      end component;
     
     --Signals required for ALU:
-    signal alu_a, alu_b: std_logic_vector(15 downto 0);
+    signal alu_a, alu_b, alu_c: std_logic_vector(15 downto 0);
     signal carry_dff_inp, zero_dff_inp: std_logic_vector();
     signal alu_sel: std_logic_vector(1 downto 0);    
     
@@ -91,17 +94,32 @@ architecture Struct of Datapath is
     signal D1,D2 D3: std_logic_vector(15 downto 0);
     signal A1,A2 A3: std_logic_vector(2 downto 0);
 
+    --Sig_Extended Signals
+    signal ImmT1_in: std_logic_vector();
     --Signals in general:
     signal SE1_out: std_logic_vector(15 downto 0);
 begin
---Component Initiate 
-    Reg_File: Register_file port map (A1, A2, A3, D3, clock);
-    ALU : ALU port map (ALU_A => alu_a, ALU_B => alu_b, ALU_C => alu_c, C_F => carry_dff_inp, Z_F => zero_dff_inp, sel => alu_sel);
-    m1 : Mux16_2x1 port map(A0 => T1_out, A1 => "0000000000000001", F => alu_a);
-    SE1 : SE7 port map(Raw => T2_out(8 downto 0), outp => SE1_out);
-    m2 : Mux16_2x1 port map(A0 => T3_out, A1 => SE1_out, F => alu_b); 
-    carry_dff: dff_en port map(clk => clock, )
+--Component Initiate for Register File
 
+    Reg_File: Register_file port map (A1, A2, A3, D3, clock,Reg_file_EN, D1,D2);
+    --Muxes for input
+    A1_Mux: Mux16_2x1 port map(loop_count, "111", A1_sel, A1);
+    A3_Mux: Mux16_4x1 port map ();
+    --Signed Extender
+    
+--Components for ALU
+    ALU : ALU port map (ALU_A => alu_a, ALU_B => alu_b, ALU_C => alu_c, C_F => carry_dff_inp, Z_F => zero_dff_inp, sel => alu_sel);
+    m1 : Mux16_2x1 port map(A0 => T1_out, A1 => "0000000000000001", sel => sel_m1, F => alu_a);
+    SE1 : SE7 port map(Raw => T2_out(8 downto 0), outp => SE1_out);
+    m2 : Mux16_2x1 port map(A0 => T3_out, A1 => SE1_out, sel => sel_m2, F => alu_b); 
+    carry_dff: dff_en port map(clk => clock, reset => reset, en => C_ctrl, q => C_flag);
+    zero_dff: dff_en port map(clk => clock, reset => reset, en => Z_ctrl, q => Z_flag);
+    
+    T1: Temp_Reg port map(DataIn => T1_in, clock => clock, Write_Enable => T1_WR, DataOut => T1_out);
+    T2: Temp_Reg port map(DataIn => T2_in, clock => clock, Write_Enable => T2_WR, DataOut => T2_out);
+    T3: Temp_Reg port map(DataIn => T3_in, clock => clock, Write_Enable => T3_WR, DataOut => T3_out);
+
+    m3: MUX16_2x1 port map(A0=> ,A1=> ,sel =>sel_m3, F=>T3_out);                --
 --
 end Struct;
     
--- SignedExt.vhd
+++ SignedExt.vhd
@@ -2,16 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 -- write the Flipflops packege declaration
-entity SignedExt is
+entity SE9 is
 port (Raw: in std_logic_vector(5 downto 0 );
     Output:out std_logic_vector(15 downto 0):="0000000000000000");
-end entity SignedExt;
+end entity SE5;
 
-architecture struct of SignedExt is
+architecture struct of SE9 is
 begin
-SE_process : process(Raw) 
-  begin
-    Output(5 downto 0) <= Raw(5 downto 0);
-    Output(15 downto 6) <= "0000000000";
-end process;
+  SE_process : process(Raw) 
+    begin
+      Output(5 downto 0) <= Raw(5 downto 0);
+      Output(15 downto 6) <= "0000000000";
+  end process;
 end struct;
